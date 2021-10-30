68ef35474e09861b228836976db40f09c2610fd1
Arushigandhi <75487137+Arushigandhi@users.noreply.github.com>
2021-10-30 23:37:12 +0530

    content rectified

--- src/pages/Adult.js
+++ src/pages/Adult.js
@@ -8,7 +8,7 @@ export default function Adult() {
             <h1 className={styles.Heading}>ADULT</h1>
             <CardComponent Category="Pregnancies" TOne="When can one get pregnant?" DOne="01" TTwo="How to get pregnant?" DTwo="02" TThree="How to maintain good pregnancy health?" DThree="03"/>
             <CardComponent Category="Contraceptives" TOne="Pre-sex methods" DOne="01" TTwo="During sex methods" DTwo="02" TThree="Post-sex methods" DThree="03"/>
-            <CardComponent Category="Understanding Menopause" TOne="Hormonal chnages during Menopause" DOne="01" TTwo="Understanding the sexual organs and how to please your partner" DTwo="02" TThree="Foreplay and kinks" DThree="03"/> 
+            <CardComponent Category="Understanding Menopause" TOne="Hormonal changes during Menopause" DOne="01" TTwo="Stages & Signs of Menopause" DTwo="02" TThree="Symptoms and Treatment" DThree="03"/> 
         </div>
 
     )
