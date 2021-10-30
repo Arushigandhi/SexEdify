474f2a23883146f15f74e0b79f3394c561cadb72
Diya Dhabade <diya.dhabade@learner.manipal.edu>
2021-10-31 02:29:20 +0530

    making quiz work

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,19 @@
-import React from 'react';
-import NavMenu from '../components/NavMenu/NavMenu';
-import HeroSection from '../components/HeroSection/HeroSection';
-import AboutUs from '../components/AboutUs/AboutUs';
-import Choose from '../components/Choose/Choose';
-
+import React from "react";
+import NavMenu from "../components/NavMenu/NavMenu";
+import HeroSection from "../components/HeroSection/HeroSection";
+import AboutUs from "../components/AboutUs/AboutUs";
+import Choose from "../components/Choose/Choose";
+import Quiz from "../components/Quiz/Quiz";
 function Home() {
-    return (
-        <div>
-        <NavMenu />
-        <HeroSection />
-        <Choose />
-        <AboutUs />
-        </div>
-    )
+  return (
+    <div>
+      <NavMenu />
+      <HeroSection />
+      <Choose />
+      <AboutUs />
+      <Quiz />
+    </div>
+  );
 }
 
-export default Home
+export default Home;
